21b0b5a89cbba83a1dab94650d7390b9321baf00
binarypheonix <21365550+binarypheonix@users.noreply.github.com>
2021-07-19 23:06:14 +0530

    deploying exp-v0.0.1

---
+++ build/precache-manifest.ff4c73cb16c8df2177b73e51210dbaf3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cc2328cdd55a7d437e6081eec16091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "119ae09c9c5a6cc86932",
+    "url": "/static/css/main.4af018f7.chunk.css"
+  },
+  {
+    "revision": "77ba809dd41baf6f61ca",
+    "url": "/static/js/2.a7e091a7.chunk.js"
+  },
+  {
+    "revision": "3718db4aeb0f4e578d2ea3a4c8b6fd69",
+    "url": "/static/js/2.a7e091a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119ae09c9c5a6cc86932",
+    "url": "/static/js/main.c1496eaf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a9dd48a803457026ce0159f2e32567bc",
+    "url": "/static/media/header-logo.a9dd48a8.svg"
+  },
+  {
+    "revision": "8cce1dcbdb9e8aa0871f25d0092cc074",
+    "url": "/static/media/link-icon.8cce1dcb.svg"
+  },
+  {
+    "revision": "d899201254e9b34d0708a0af1db9c642",
+    "url": "/static/media/splash-logo.d8992012.svg"
+  },
+  {
+    "revision": "b5be31f680217869cb6dd73e5cb4c66c",
+    "url": "/static/media/tezos-sym.b5be31f6.svg"
+  },
+  {
+    "revision": "1a72e9f43d27014cd6a4abb3ac4b9cca",
+    "url": "/static/media/wallet.1a72e9f4.svg"
+  }
+]);
